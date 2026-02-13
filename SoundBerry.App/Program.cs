c2446e4764b4a0af2ab33e04c6dc9672d6081cd2
= <=>
2026-02-12 18:37:41 -0600

    added sqlite

--- SoundBerry.App/Program.cs
+++ SoundBerry.App/Program.cs
@@ -1,9 +1,85 @@
-﻿namespace SoundBerry.App;
+﻿using NAudio.Wave;
 
-class Program
+namespace SoundBerry.App;
+
+internal static class Program
 {
-    static void Main(string[] args)
+    private static WaveOutEvent _player;
+    private static AudioFileReader _audioFile;
+
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Clear();
+
+        var selectedIndex = 0;
+        var key = ConsoleKey.None;
+
+        var items = new List<string>
+        {
+            "sample1.mp3",
+            "sample2.mp3",
+            "sample3.mp3"
+        };
+
+
+        while (true)
+        {
+            Console.Clear();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (i == selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.WriteLine($"{items[i]}");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine($"{items[i]}");
+                }
+            }
+
+            key = Console.ReadKey().Key;
+
+            if (key == ConsoleKey.UpArrow && selectedIndex > 0)
+                selectedIndex--;
+
+            if (key == ConsoleKey.DownArrow && selectedIndex < items.Count - 1)
+                selectedIndex++;
+
+            if (key == ConsoleKey.Enter)
+            {
+                PlayFile(items[selectedIndex]);
+            }
+
+            if (key == ConsoleKey.Escape)
+            {
+                StopPlayer();
+            }
+        }
+    }
+
+    private static void StopPlayer()
+    {
+        _player?.Stop();
+        _player?.Dispose();
+    }
+
+    private static void PlayFile(string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath)) return;
+
+        _player?.Stop();
+        _player?.Dispose();
+
+        _audioFile?.Dispose();
+
+        _audioFile = new AudioFileReader(filePath);
+        _player = new WaveOutEvent();
+
+        _player.Init(_audioFile);
+        _player.Play();
     }
 }
---
+++ SoundBerry.App/Track.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace SoundBerry.App;
+
+public class Track
+{
+    private static readonly string _connectionString = "Data Source=soundberry.db;";
+
+    public int Id { get; set; } = 0;
+    public string? Title { get; set; } = string.Empty;
+    public string? Artist { get; set; } = string.Empty;
+    public TimeSpan? Duration { get; set; } = TimeSpan.Zero;
+
+    private static SqliteConnection OpenConnection()
+    {
+        var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        return connection;
+    }
+
+    private static Track LoadReader(SqliteDataReader reader)
+    {
+        return new Track
+        {
+            Id = Convert.ToInt32(reader["Id"]),
+            Artist = reader.IsDBNull(reader.GetOrdinal("artist")) ? null : reader["artist"].ToString(),
+            Title = reader.IsDBNull(reader.GetOrdinal("title")) ? string.Empty : reader["title"].ToString(),
+            Duration = reader.IsDBNull(reader.GetOrdinal("duration"))
+                ? TimeSpan.Zero
+                : TimeSpan.FromSeconds(reader.GetInt32(reader.GetOrdinal("duration")))
+        };
+    }
+}
