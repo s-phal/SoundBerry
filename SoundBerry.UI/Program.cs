f78f0e8d6fa11e56172afc5d781772ae84e7d017
= <=>
2026-02-12 22:36:43 -0600

    added sqlite

---
+++ SoundBerry.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using SoundBerry.DataAccess;
+using SoundBerry.DataAccess.Models;
+
+namespace SoundBerry.UI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DbConfig.CreateTableIfNotExists();
+    }
+}
